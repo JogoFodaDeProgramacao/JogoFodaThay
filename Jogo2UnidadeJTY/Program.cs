fa49d3e256d304c2e587b6b5e8adfdd18f88bc73
Th2y <56850377+Th2y@users.noreply.github.com>
2019-10-21 19:58:07 -0300

    Add files via upload

---
+++ Jogo2UnidadeJTY/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Jogo2UnidadeJTY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Olá! Seja bem vindo(a)!");
+            Console.WriteLine("Como gostaria de ser chamado(a)?");
+            string nome = Console.ReadLine();
+            Console.WriteLine(nome + " que belo nome! Vamos então começar a nossa aventura");
+            Console.Clear();
+
+            Console.WriteLine("Estamos no ano 2500, vivemos viajando entre os planetas apenas por diversão"); // Narração
+            Console.WriteLine("AHHHH O QUE ESTÁ ACONTECENDO?! SOCORRO!"); // Astronauta
+            Console.WriteLine("Fique calmo, estamos tentando te localizar e logo enviaremos o que for preciso para que você volte para casa"); // Estação de controle
+            Console.WriteLine("NÃO! AQUI NÃO! "); // Astronauta
+            Console.WriteLine("Cai e a nave não consegue mais voar"); // Narração
+            Console.WriteLine("Tudo bem, tudo bem, tenho que me acalmar"); // Astronauta
+            Console.WriteLine("No manual dizia que eu não posso ir para muito longe, se as peças quebraram, todas devem estar por perto"); // Astronauta
+            Console.WriteLine("Estou na Terra, então tenho 5 dias de suprimento, enquanto isso tenho que tomar cuidado."); // Astronauta
+            Console.WriteLine("Espero conseguir encontrar tudo rápido, não posso demorar"); // Astronauta
+            Console.WriteLine("");
+        }
+    }
+}
