ad4cb866851df357adaa85268ea0278c66f2ffb2
Thayane Carvalho dos Santos <56850377+Th2y@users.noreply.github.com>
2019-11-25 16:08:08 -0300

    Add files via upload

---
+++ CosmicEscape/Creditos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoPica
+{
+    class Creditos
+    {
+        public void FimDeJogo()
+        {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("-------------------------------------------------------- Agradecimentos --------------------------------------------------------");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(" Agradecemos ao prof por ter dado 8,0 adiantado no nosso jogo. Obrigado prof!");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------------------------------------------------------- Créditos -----------------------------------------------------------");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("                                                        Desenvolvedores: ");
+            Console.WriteLine("                                     -------------------------------------------------------");
+            Console.WriteLine("                                                 José Antônio de Almeida Filho");
+            Console.WriteLine("                                                  Thayane Carvalho dos Santos");
+            Console.WriteLine("                                                 Yasmin Alexandre Araujo Barros");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("                                                           Orientador: ");
+            Console.WriteLine("                                     -------------------------------------------------------");
+            Console.WriteLine("                                                     Felipe Valadao Goulart");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("                                                             Matéria: ");
+            Console.WriteLine("                                     -------------------------------------------------------");
+            Console.WriteLine("                                                Programação Aplicada a Jogos Digitais");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("                                                             Curso: ");
+            Console.WriteLine("                                     -------------------------------------------------------");
+            Console.WriteLine("                                                 Tecnologia em Jogos Digitais");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("                                                      Instituição de ensino: ");
+            Console.WriteLine("                                     -------------------------------------------------------");
+            Console.WriteLine("                                                               UNIT");
+
+        }
+    }
+}
